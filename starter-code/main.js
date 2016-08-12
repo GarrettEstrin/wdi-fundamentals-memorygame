08a1dd2b94cbd2bcb3f0e8ed73fb740cce5fb4ea
Garrett Estrin <garrettestrin@Garretts-MacBook-Pro.local>
2016-08-11 18:01:45 -0700

    Game works.  Clear board after wrong does not

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,41 +1,47 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = 'king';
-var cardFour = "king";
+// game cards
+var cards = ['queen', 'queen', 'king', 'king'];
 
-if (cardTwo === cardFour) {
-  alert('You found a match!')
-} else {
-  alert("Sorry, try again.")
+// cars in play
+var cardsInPlay = [];
+
+
+
+// find the board
+var board = document.getElementById('game-board');
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    board.appendChild(cardElement);
+  }
+
+}
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  console.log(this.getAttribute('data-card'));
+  if (this.getAttribute('data-card') === 'king') {
+    this.innerHTML = "<img src='king.jpg'>"; 
+  } else {
+    this.innerHTML = "<img src='queen.jpg'>"; 
+  }
+  //this checks the number of cards in play
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    // clear cards - NOT WORKING - NEED FIX
+  cardsInPlay = [];
+  }
 }
 
-if (cardOne === cardTwo) {
-  alert('You found a match!')
-} else {
-  alert("Sorry, try again.")
+function isMatch(cards) {
+  // alert
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
 }
 
-if (cardOne === cardThree) {
-  alert('You found a match!')
-} else {
-  alert("Sorry, try again.")
-}
-
-if (cardOne === cardFour) {
-  alert('You found a match!')
-} else {
-  alert("Sorry, try again.")
-}
-
-if (cardTwo === cardThree) {
-  alert('You found a match!')
-} else {
-  alert("Sorry, try again.")
-}
-
-if (cardThree === cardFour) {
-  alert('You found a match!')
-} else {
-  alert("Sorry, try again.")
-}
+createBoard();
